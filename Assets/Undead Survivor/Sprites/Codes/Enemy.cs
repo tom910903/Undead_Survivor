5c4c17e4beb86370ce743dca367181624c96113e
tom910903 <tom910903@gmail.com>
2023-06-24 15:27:52 +0900

    Add Enemy

--- Assets/Undead Survivor/Sprites/Codes/Enemy.cs
+++ Assets/Undead Survivor/Sprites/Codes/Enemy.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public float health;
+    public float maxHealth;
+    public RuntimeAnimatorController[] aniCon;
     public Rigidbody2D target;
 
-    bool isLive = true;
+    bool isLive;
 
     Rigidbody2D rigid;
+    Animator ani;
     SpriteRenderer spriter;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
     }
 
@@ -31,5 +36,20 @@ public class Enemy : MonoBehaviour
     void LateUpdate()
     {
         spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        ani.runtimeAnimatorController = aniCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
     }
 }
--- Assets/Undead Survivor/Sprites/Codes/GameManager.cs
+++ Assets/Undead Survivor/Sprites/Codes/GameManager.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public PoolManager pool;
     public Player player;
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
 
     void Awake()
     {
         instance = this;
     }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+    }
+
 }
